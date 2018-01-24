43ff97c9dbd07ad63f55e8c23215cd0cd5399d3a
Jorge Jaular <jordijaulas13@gmail.com>
2018-01-24 11:07:15 +0100

    Cambios realizados en la inicialización de atributos en el constructor y métodos getter creados

--- SerieTV.java
+++ SerieTV.java
@@ -9,17 +9,41 @@ public class SerieTV
 {
     // Título de la película.
     private String titulo;
-    // El número de la temporada.
-    private int temporada;
+    // El número de temporadas.
+    private int numTemporadas;
     // La fecha de estreno de la serie.
     private LocalDate fechaEstreno;
     /**
-     * Constructor for objects of class SerieTV
+     * Constructor de la clase SerieTV
      */
-    public SerieTV(String titulo)
+    public SerieTV(String titulo, int numTemporadas, int ano, int mes, int dia)
     {
         this.titulo = titulo;
-        temporada = 0;
-        fechaEstreno = null;
-    }    
+        this.numTemporadas = numTemporadas;
+        fechaEstreno = LocalDate.of(ano, mes, dia);
+    }
+    
+    /**
+     * Devuelve el título de la serie.
+     */
+    public String getTitulo()
+    {
+        return titulo;
+    }
+    
+    /**
+     * Devuelve el número de temporadas de una serie.
+     */
+    public int getNumTemporadas()
+    {
+        return numTemporadas;
+    }
+    
+    /**
+     * Devuelve la fecha de estreno de la serie.
+     */
+    public LocalDate getFechaEstreno()
+    {
+        return fechaEstreno;
+    }
 }
