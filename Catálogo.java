7f4c51be098ca91b7dcca0e9c16664eeec1b6fce
Jorge Jaular <jordijaulas13@gmail.com>
2018-01-24 10:41:50 +0100

    Clase Catálogo y Clase SerieTV creadas.

---
+++ Catálogo.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Catálogo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Catálogo
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Catálogo
+     */
+    public Catálogo()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ SerieTV.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class SerieTV here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SerieTV
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class SerieTV
+     */
+    public SerieTV()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
